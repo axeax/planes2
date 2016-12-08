e5b56996d6d3bd7c3893eea80c2e7560f904a674
axeax <axeax@mail.ru>
2016-12-08 21:44:39 +0300

    перенес паттерны в rules.js

---
+++ newjs/rules.js
@@ -0,0 +1,25 @@
+/* NAME RULES */
+/*
+
+class 			ClassName
+object class 	_ObjectName
+method			_methodName
+prototype		_prototypeMethodName
+var, let 		char_variableName, int_variableName
+1|2|3...		case_variableName (один из нескольких статичных параметров)
+function 		f_functionName
+object			o_objectName
+array			a_arrayName
+DOM 			$_domName
+const			CONST_NAME_NAME
+
+mix:
+DOM array 		a_$_arrayName
+const array 	A_ARRAY_NAME
+DOM function 	f_$_functionName
+DOM method 		_$_methodName
+
+objects need method _comment
+
+*/
+/* /NAME RULES */

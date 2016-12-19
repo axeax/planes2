c9d711daa5f65a0954153ab49cfa2fb6c64d4e6e
axeax <axeax@mail.ru>
2016-12-19 16:33:01 +0300

    добавил пример конфига для одного из параметров самолета и метод, выявляющий несоответсвтия в имени переменной и её содержании

--- newjs/global_config.js
+++ newjs/global_config.js
@@ -1,9 +1,13 @@
 /* Файл с глобальной конфигурацией, работающий и на клиенте и на сервере */
-const CONFIG = {
-a_medails = [
-	{
+/* НЕ имеет методов работы с интерфейсом. Для работы с интерфейсом существует отдельный класс */
+/* Существующие методы работают только с самим объектом конфига для объединения повторяющихся данных */
 
-		// пример одной медали
+"use strict";
+
+var Config = function(){
+
+this.a_medails = [	// все медали
+	{ 	// пример одной медали
 
 		char_nameText: 			'name',
 		char_questText: 		'сыграть 10 раз | добить пулеметом',
@@ -13,11 +17,11 @@ a_medails = [
 		int_fullProgress: 		10,
 		a_award: 				[
 			{
-				char_type: 			metall,
+				char_type: 			'metall',
 				int_value: 			10
 			},
 			{
-				char_type: 			stars,
+				char_type: 			'stars',
 				int_value: 			20
 			}
 		],
@@ -25,13 +29,11 @@ a_medails = [
 		char_wallImageSrc: 		'/path/to/file',
 		char_interfaceImageSrc: '/path/to/file'
 
-	},
+	}
 	// остальные медали
-],
-a_weapons = [
-	{
-
-		// пример одной ракеты
+];
+this.a_weapons = [	// все оружие
+	{ 	// пример одной ракеты
 		// там, где представлен массив значений, они разделяются на wl1, wl2 и wl3
 
 		char_name: 			'Пулемет',
@@ -47,29 +49,26 @@ a_weapons = [
 		int_price: 			1, // стоимость в звездах
 		int_purchase: 		300, // количество при покупке
 		o_wlPrice: 			{ // стоимость каждого уровня оружия (всего 2, т.к. Wl1 уже куплено)
-			int_metall: 		[10, 15],
-			int_silicon: 		[10, 15],
-			int_tnt: 			[10, 15],
-			int_diamonds: 		[10, 15]
+			a_metall: 			[10, 15],
+			a_silicon: 			[10, 15],
+			a_tnt: 				[10, 15],
+			a_diamonds: 		[10, 15]
 		},
 		o_availableOnPlanes: { // доступно на самолетах
-			bool_standard: 			true,
-			bool_fast: 				true,
-			bool_protected: 		true,
-			bool_powerful: 			true,
-			bool_maneuver: 			true,
-			bool_secret: 			true
+			bool_standard: 		true,
+			bool_fast: 			true,
+			bool_protected: 	true,
+			bool_powerful: 		true,
+			bool_maneuver: 		true,
+			bool_secret: 		true
 		}, 
 		char_about: 		'Пулемёт&nbsp;&mdash; оружие &laquo;быстрого огня&raquo;. Скорость стрельбы очень высокая, пуля летит достаточно быстро, но&nbsp;точность и&nbsp;убойность оставляют желать лучшего. Больше всего подходит для &laquo;добивания&raquo; соперника. При попадании не&nbsp;оказывает никакого дополнительного воздействия на&nbsp;самолёт.'
 
-
-	},
+	}
 	// остальные ракеты
-],
-a_skills = [
-	{
-
-		// пример одного скилла
+];
+this.a_skills = [	// все скиллы
+	{	// пример одного скилла
 		// у скиллов нету Wl
 
 		char_name: 			'Аптечка',
@@ -81,22 +80,20 @@ a_skills = [
 		int_price: 			1, // стоимость в звездах
 		int_purchase: 		5, // количество при покупке
 		o_availableOnPlanes: { // доступно на самолетах
-			bool_standard: 			true,
-			bool_fast: 				true,
-			bool_protected: 		true,
-			bool_powerful: 			true,
-			bool_maneuver: 			true,
-			bool_secret: 			true
+			bool_standard: 		true,
+			bool_fast: 			true,
+			bool_protected: 	true,
+			bool_powerful: 		true,
+			bool_maneuver: 		true,
+			bool_secret: 		true
 		}, 
 		char_about: 		'Аптечка восстанавливает 10&nbsp;жизней прямо во&nbsp;время боя. Рекомендуется использовать когда количество жизней уже меньше&nbsp;25.'
 
-	},
+	}
 	// остальные скиллы
-],
-a_planes = [
-	{
-
-		// пример одного самолета
+];
+this.a_planes = [	// все самолеты
+	{	// пример одного самолета
 		// там, где представлен массив значений, они разделяются на mk1, mk2 и mk3
 
 		char_name: 				'Самолет курсанта',
@@ -115,44 +112,99 @@ a_planes = [
 			a_rate: 				[1, 1.1, 1.2]
 		},
 		o_price: 				{ // стоимость каждого Mk
-			int_star: 				[100, 200, 300],
-			int_metall: 			[5, 10, 15],
-			int_silicon: 			[5, 10, 15],
-			int_tnt: 				[5, 10, 15],
-			int_diamonds: 			[5, 10, 15]
+			a_star: 				[100, 200, 300],
+			a_metall: 				[5, 10, 15],
+			a_silicon: 				[5, 10, 15],
+			a_tnt: 					[5, 10, 15],
+			a_diamonds: 			[5, 10, 15]
 		},
 		o_requirements: 		{ // требования для покупки каждого Mk
-			int_rating: 			[10000, 15000, 20000],
-			int_technology1: 		[1,2,3], // микропроцессоры
-			int_technology2: 		[1,2,3], // турбины
-			int_technology3: 		[1,2,3], // строительные материалы
-			int_technology4: 		[1,2,3], // система наведения
-			int_technology5: 		[1,2,3], // искусственный интеллект
+			a_rating: 				[10000, 15000, 20000],
+			a_technology1: 			[1,2,3], // микропроцессоры
+			a_technology2: 			[1,2,3], // турбины
+			a_technology3: 			[1,2,3], // строительные материалы
+			a_technology4: 			[1,2,3], // система наведения
+			a_technology5: 			[1,2,3], // искусственный интеллект
 		},
-		o_availableWeapons: 	{ // инициализируется функцией _a_planes__getAvailableWeapons
+		o_availableWeapons: 	{ // доступное оружие на самолете инициализируется функцией _getAvailableWeaponsAndSkillsForPlanes
+			/*
 
+			@bool_A: 				true,
+			@bool_B: 				true,
+			@bool_C: 				true,
+			@bool_D: 				true,
+			@bool_E: 				false,
+			@bool_F: 				false,
+			@bool_G: 				false,
+			@bool_H: 				false
+
+			*/
 		},
-		
+		o_availableSkills: 		{ // доступные скиллы на самолете инициализируется функцией _getAvailableWeaponsAndSkillsForPlanes
+			/*
 
-	},
+			@bool_I: 				true,
+			@bool_J: 				true,
+			@bool_K: 				true,
+			@bool_L: 				true,
+			@bool_M: 				false,
+			@bool_N: 				false,
+			@bool_O: 				false,
+			@bool_P: 				false
+
+			*/
+		}	
+
+	}
 	// остальные самолеты
-],
+];
+this.a_params = [	// все параметры самолета. Параметры распостраняются на все самолеты сразу
+	{	// пример одного параметра
 
-// функция преобразует o_availableOnPlanes в o_availableWeapons, принимает текущий номер самолета
-_a_planes__getAvailableWeapons: 	function(){
+		char_nameText: 		'скорость',
+		char_nameClass: 	'speed',
+		float_default: 		10,
+		float_maxValue: 	30,
+		a_priceStars: 		[], // массив цен по каждому шагу, формируется функцией _getPricesParams
+		a_priceRating: 		[], // массив необходимого рейтинга для покупки, формируется функцией _getPricesParams
 
-	for(let p = 0; p < this.a_planes.length, p++){
+	}
+];
 
-		for(let w; w < this.a_weapons.length; w++){
+// функция преобразует o_availableOnPlanes из a_skills и a_weapons в o_availableWeapons и в o_availableSkills
+this._getAvailableWeaponsAndSkillsForPlanes = function(){
+
+	for (let p = 0; p < this.a_planes.length; p++) {
+
+		// оружие
+		for(let w = 0; w < this.a_weapons.length; w++){
 
 			// имя класса оружия
-			let char_className = this.a_weapons[w].char_class;
+			let char_className = 'bool_' + this.a_weapons[w].char_class;
 
 			// доступно ли оружие на текущем самолете
-			let bool_enabled = this.a_weapons[w].o_availableOnPlanes['bool_' + this.this.a_planes[p].char_class];
+			// оружие.доступностьНаСамолетах[имяСамолетаБеретсяТекущее];
+			let bool_enabled = this.a_weapons[w].o_availableOnPlanes['bool_' + this.a_planes[p].char_class];
 
 			// присваиваем текущему самолету доступность текущего оружия
 			this.a_planes[p].o_availableWeapons[char_className] = bool_enabled;
+
+			console.log(char_className, bool_enabled);
+
+		}
+
+		// скиллы
+		for(let s = 0; s < this.a_skills.length; s++){
+
+			// имя класса скилла
+			let char_className = 'bool_' + this.a_skills[s].char_class;
+
+			// доступен ли скилл на текущем самолете
+			// скилл.доступностьНаСамолетах[имяСамолетаБеретсяТекущее];
+			let bool_enabled = this.a_skills[s].o_availableOnPlanes['bool_' + this.a_planes[p].char_class];
+
+			// присваиваем текущему самолету доступность текущего скилла
+			this.a_planes[p].o_availableSkills[char_className] = bool_enabled;
 
 		}
 
@@ -160,5 +212,185 @@ _a_planes__getAvailableWeapons: 	function(){
 
 	return;
 
-},
+}; // /_getAvailableWeaponsAndSkillsForPlanes
 
+// функция рассчитывает все цены и рейтинги для параметров самолета
+this._getPricesParams = function(){
+
+	for(let count_params = 0; count_params < this.a_params.length; count_params++){
+
+		let link_currentParam =  this.a_params[count_params];
+
+		for(let count_step = link_currentParam.float_default; count_step < link_currentParam.float_maxValue; count_step++){
+
+			// инициализируем значения которые в конце добавятся в массив
+			let int_stars, int_rating;
+
+			// рассчитываем количество звезд для покупки исходя из текущего шага
+			function f_getStars(count_step){
+
+				// дефолная начальная стоимость - 10 звезд
+				let int_stars;
+
+				// рассчет по формуле
+				int_stars = (count_step * count_step) / 10;
+
+				// округляем в бОльшую сторону
+				int_stars = Math.ceil(int_stars);
+
+				return int_stars;
+
+			}
+
+			// рассчитываем рейтинг необходимый для покупки исходя из текущего шага
+			function f_getRating(count_step){
+
+				// дефолная начальная стоимость - 10 звезд
+				let int_rating;
+
+				// рассчет по формуле
+				int_rating = Math.pow(int_rating, 4) / 10;
+
+				// округляем в бОльшую сторону
+				int_rating = Math.ceil(int_rating);
+
+				return int_rating;
+
+			}
+
+			int_stars = f_getStars(count_step);
+			int_rating = f_getRating(count_step);
+
+			// добавляем рассчитанное значение в массив стоимости (звезды)
+			link_currentParam.a_priceStars[count_step] = int_stars;
+			// добавляем рассчитанное значение в массив требований к рейтингу
+			link_currentParam.a_priceRating[count_step] = int_rating;
+
+		}
+
+	}
+
+	return;
+
+}; // /_getPricesParams
+
+// проверяет соответствие имя_переменной - тип_переменной
+this._TEST = function(){
+
+	// принимает значение и имя переменной, возвращает тип и соответствие имени
+	function f_getType(value, name){
+
+		// возвращяемый объект
+		let o_return = {
+			char_type: '', // тип переменной
+			bool_equivalent: '' // совпадает и именеи или нет
+		}
+
+		// массив соответствий
+		let o_equivalents = {
+
+			'array': 'a_',
+			'undefined': 'undefined_',
+			'boolean': 'bool_',
+			'string': 'char_',
+			'number': ['int_', 'float_'],
+			'object': 'o_',
+			'function': ['f_', '_']
+
+		}
+
+		// назначаем текущий тип
+		o_return.char_type = typeof value;
+
+		// массив проходит отдельно
+		if(Array.isArray(value)){ 
+
+			o_return.char_type = 'array'; 
+
+		}
+
+		// сюда записывается тип переменной из имени
+		let char_nameType = name.split('_')[0] + '_';
+
+		// проверяем соответствие
+		if(o_equivalents[o_return.char_type].indexOf(char_nameType) + 1){
+
+			o_return.bool_equivalent = true;
+
+		}
+		else{
+
+			o_return.bool_equivalent = false;
+
+		}
+
+		return o_return;
+
+	}
+
+	// циклом проходим по свойствам объекта
+	for(let char_prop in this){
+
+		// проверка на собственное значение, а не прототип
+		if(this.hasOwnProperty(char_prop)){
+
+			// вызываем функцию, возвращающую реальный тип и соответствие
+			let o_type = f_getType(this[char_prop], char_prop);
+
+			// предупреждаем о несоответствии
+			if(!o_type.bool_equivalent){
+
+				console.log('!!!');
+
+			}
+
+			// выводим результат теста
+			console.log(`${char_prop}: ${o_type.char_type}, ${o_type.bool_equivalent}`);
+
+			// если проверяемое значение является объектом - заглядываем внутрь (рекурсивно)
+			if(o_type.char_type == 'object'){
+
+				CONFIG._TEST.call(this[char_prop]);
+
+			}
+			// если проверяемое значение является массивом - заглядываем внутрь (рекурсивно)
+			else if(o_type.char_type == 'array'){
+
+				// внутри циклом обходим массив
+				for(let i = 0; i < this[char_prop].length; i++){
+
+					// и проверяем. Если элемент массива - объект, то заглядываем туда еще раз
+					if(typeof this[char_prop][i] == 'object'){
+
+						CONFIG._TEST.call(this[char_prop][i]);
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+	return;
+
+};
+
+this.o_testObject = {
+	int_a: 1
+};
+
+} // / Config
+
+var CONFIG = new Config();
+
+// инициализируем доступность оружия и бонусов на самолетах
+CONFIG._getAvailableWeaponsAndSkillsForPlanes();
+
+// инициализация цен и необходимого рейтинга для параметров
+CONFIG._getPricesParams();
+
+// тестируем на соответствие имена переменных
+CONFIG._TEST();
